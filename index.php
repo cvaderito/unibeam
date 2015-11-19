54ed834cfb47856d42bd51f38289231ba073ae46
cvaderito <limav.wilmer@gmail.com>
2015-11-19 17:13:15 -0500

    Carga desde la base de datos y metodos de inserción de datos

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
         <?php
             require_once 'modelos/config.php';
             require_once 'routes.php';
-            echo 'holA';
         ?>
         <script src="assets/js/jquery.js"></script>
         <script src="assets/js/bootstrap.min.js"></script>
